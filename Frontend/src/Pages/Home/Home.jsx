02cadf88e802b8e40f6e5e9bbf377f917d008859
dejiobasan <dejiobasan@yahoo.com>
2023-10-12 19:13:51 +0100

    .

---
+++ Frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+function Home() {
+    return (
+        <div className="container">
+            <h1 className="text-slate-600">
+                Hello
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ Frontend/src/Pages/Home/Navbar.jsx
@@ -0,0 +1,10 @@
+
+function Navbar() {
+    return (
+        <div className="bg-slate-600">
+            <h1>Welcome</h1>
+        </div>
+    );
+}
+
+export default Navbar;
